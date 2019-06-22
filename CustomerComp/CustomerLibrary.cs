2f8fba194d285a023d9bed6d69f90a33a0c053b6
alecurobert05 <infoalecu@gmail.com>
2019-06-22 16:34:41 +0300

    Update CustomerLibrary.cs
    Add Validate method.

--- CustomerComp/CustomerLibrary.cs
+++ CustomerComp/CustomerLibrary.cs
@@ -6,14 +6,6 @@ using System.Threading.Tasks;
 
 namespace CustomerComp
 {
-    public class Lead
-    {
-        public string LeadName { get; set; }
-        public string PhoneNumber { get; set; }
-        public decimal BillAmount { get; set; }
-        public DateTime BillDate { get; set; }
-        public string Address { get; set; }
-    }
     public class Customer
     {
         public string CustomerName { get; set; }
@@ -21,5 +13,40 @@ namespace CustomerComp
         public decimal BillAmount { get; set; }
         public DateTime BillDate { get; set; }
         public string Address { get; set; }
+
+        //"Validate” -> metoda care valideaza propietatile de mai sus 
+        public virtual void Validate()
+        {
+            if (CustomerName.Length == 0)
+            {
+                throw new Exception("Customer Name is required");
+            }
+            if (PhoneNumber.Length == 0)
+            {
+                throw new Exception("Phone number is required");
+            }
+            if (BillAmount > 0)
+            {
+                throw new Exception("Bill is required");
+            }
+            if (BillDate >= DateTime.Now)
+            {
+                throw new Exception("Bill date  is not proper");
+            }
+        }
+    }
+    public class Lead : Customer
+    {
+        public override void Validate()
+        {
+            if (CustomerName.Length == 0)
+            {
+                throw new Exception("Customer Name is required");
+            }
+            if (PhoneNumber.Length == 0)
+            {
+                throw new Exception("Phone number is required");
+            }
+        }
     }
 }
